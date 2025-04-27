7c6acc86f9eef5e7129c02477fbedc310ef0dea6
Humphrey47Bot MD <humphreymbise68@gmail.com>
2025-04-27 11:50:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,110 @@
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+ <h1 align="center"> ZEZE47 MD </h1>
+
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+
+ 
+<p align="center">
+  <marquee behavior="scroll" direction="right">
+    <h1 style="color:blue;">ZEZE47 MD</h1>
+    <h2 style="color:green;">BEST WHATSAPP BOT</h2>
+    <h3 style="color:red;">CREDIT BY MR ZEZE47 TECH</h3>
+  </marquee>
+</p>
+<a><img 
+<p align="center"> Introducing ZEZE47 MD, A Simple WhatsApp user BOT, Created by zeze47 md.ping ✍️Animation">
+</p>
+
+  <a href="https://ibb.co/N6NMDtn"><img src="https://files.catbox.moe/qddjrn.jpg" alt="01" border="0" /></a>                     
+<a><img src='https://i.imgur.com/LyHic3i.gif'/>&</a> 
+
+
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+
+
+#### SETUP
+
+1. Fork the repo
+    <br>
+<a href='https://github.com/Zezeboy/ZEZE47-MD-V1/fork' target="_blank"><img alt='Fork repo' src='https://img.shields.io/badge/Fork Repo-100000?style=for-the-badge&logo=scan&logoColor=white&labelColor=black&color=black'/></a>
+
+
+
+2. Get Session ID (By pairing code)
+   > 
+     <a href='https://alone-pairing-code.onrender.com/pair' target="_blank"><img alt='SESSION ID' src='https://img.shields.io/badge/Session_id-100000?style=for-the-badge&logo=scan&logoColor=white&labelColor=black&color=black'/></a>
+
+
+
+3. Deploy on heroku
+    <br>
+<a href='https://dashboard.heroku.com/new?template=https://github.com/Zezeboy/ZEZE47-MD-V1' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/DEPLOY-100000?style=for-the-badge&logo=scan&logoColor=white&labelColor=black&color=black'/></a>
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+
+   
+## Supported Versions Node Versions to run this bot
+
+Please Use Node Version Higher to Get The Best Performance.
+
+| Version | Supported          |
+| ------- | ------------------ |
+| 14.x   | :x: |
+| 16.x   | ❗                |
+| 18.x   | :white_check_mark: |
+| 20.x   | ✅                |
+
+## Support 
+<a href="https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r" target="_blank">
+    <img alt="whatsapp Group" src="https://img.shields.io/badge/ Whatsapp Support Channel -25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+  </a>
+</p>
+
+
+HOW TO REACH THE OWNER? 
+ 
+   
+   <a href="https://wa.me+255682937675">
+    <img src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+  </a>&nbsp;&nbsp;
+   <a
+
+    ## Ask any thing
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+
+
+
+## Makesure you follow my channel for latest updates 
+ [`WA CHANNEL`](https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r)
+
+
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+   
+   
+## Thankyou for choosing ZEZE47 MD bot 
+
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+
+## Contributions
+
+
+Contributions to *ZEZE47-MD* are welcome! If you have ideas for new features, improvements, or bug fixes, feel free to open an issue or submit a pull request.
+
+# Security Notice
+ZEZE47 Md is safe for your WhatsApp and heroku
+
+
+
+## powered by ZEZE47 MD
+
+
+
+The *ZEZE47-MD* is released under the [MIT License](https://opensource.org/licenses/MIT).
+
---
+++ commandes/vc_files.js
@@ -0,0 +1,276 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../framework/dl/Function");
+const { default: axios } = require('axios');
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+
+
+zokou({ nomCom: 'vcf',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '📄', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_please wait..._*`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'getall',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '😎', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_getting all members_*`) 
+
+   
+
+
+  }
+);
+
+
+
+zokou({ nomCom: 'channel',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🍁', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r`) 
+
+   
+
+
+  }
+);
+zokou({ nomCom: 'Zeze',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🍁', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`ʜᴇʟʟᴏ.... ɪ'ᴍ ʜᴀᴘᴘʏ ᴛᴏ sᴇᴇ ʏᴏᴜ
+
+ZEZE47 ᴏᴡɴᴇʀ sᴀʏs ᴍᴏsᴛ ᴏғ ᴜᴘᴅᴀᴛᴇs ᴀʀᴇ ᴀʀᴏᴜɴᴅ ᴛʜᴇ ᴄᴏʀɴᴇʀ... ʏᴏᴜ ᴡɪʟʟ ᴇɴᴊᴏʏ ᴍᴏʀᴇ ᴄᴏᴍᴍᴀɴᴅs ᴀs ʜᴇ ᴀᴅᴅs ᴛʜᴇᴍ...
+
+....ᴜsᴇ ᴍᴇ ᴄᴀʀᴇғᴜʟʟʏ 
+
+ɪ ʟᴏᴠᴇ ʏᴏᴜ`) 
+
+   
+
+
+  }
+);
+
+zokou({ nomCom: 'channel1',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🍁', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`top here to join my second channel https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'update',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🤷', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_ZEZE47 MD is running...... Love it_*`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'vision',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🔎', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_ᴏᴜʀ ᴍɪssɪᴏɴ ɪs ᴛᴏ ʟᴇᴛ ʏᴏᴜ ᴇɴᴊᴏʏ ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ.... ❣️ ɪ ʟᴏᴠᴇ ʏᴏᴜ _*`) 
+
+   
+
+
+  }
+);
+
+
+  
+zokou({ nomCom: 'group1',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🤫', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`top a link to join boniphace group  https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r`) 
+
+   
+
+
+  }
+)
+
+
+zokou({ nomCom: 'scriptkidies',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🐅', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`top here to Alone script kiddies group https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r`) 
+
+   
+
+
+  }
+)
+
+
+zokou({ nomCom: 'Zeze',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🤷', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_YESS....I'M LISTENING TO YOU_*`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'me',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🤷', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*check the developer via https://wa.me/c/255682937675*`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'problem',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🤷', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*┏━━━━━━━━━━━━━━
+┃ZEZE-MD 🎉🎉🎉 
+| NEVER 
+┃DIE🔥
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❶ || Creator = 𖥘 ZEZE TECH 𖥘
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❷ || WhattsApp Channel = https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+Please Follow My Support Channel
+Wanna talk to me?👉 https://wa.me/c/255682937675 👈
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+© *ZEZE TECH*`) 
+
+   
+
+
+  }
+);
+
