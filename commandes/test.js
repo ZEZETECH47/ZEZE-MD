17cc52498f5e93769693c53c5457a0514d4f86a7
Humphrey47Bot MD <humphreymbise68@gmail.com>
2025-04-28 12:45:38 +0300

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "test", reaction: "📎", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Hello my name is  *ZEZE47 MD* \n\n ' + "i'm a whatsapp bot multi-device created ";
-    let d = ' by *ZEZE TECH*';
+    let z = 'Hello my name is  *𝐙𝐄𝐙𝐄𝟒𝟕-𝐌𝐃* \n\n ' + "i'm a whatsapp bot multi-device created ";
+    let d = ' by *𝘡𝘌𝘡𝘌47 𝘛𝘌𝘊𝘏*';
     let varmess = z + d;
     var img = 'https://i.imgur.com/jE8eQsP.jpeg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
