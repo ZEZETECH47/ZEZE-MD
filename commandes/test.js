398e4aa7ed9bd76d42d62c4fea089bf122d16b7e
Humphrey47Bot MD <humphreymbise68@gmail.com>
2025-04-28 09:07:22 +0300

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -19,8 +19,8 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *ALONE* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *Topu tech*'
+   let z ='Salut je m\'appelle *ZEZE47* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *ZEZE TECH*'
       let varmess=z+d
       var img='https://i.imgur.com/jE8eQsP.jpeg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
