a942194df93bdf40952f8ec4ef064ec65ced2dc1
Humphrey47Bot MD <humphreymbise68@gmail.com>
2025-04-29 13:28:28 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*•🛰️𝘡𝘌𝘡𝘌47 𝘛𝘌𝘊𝘏• ✍️𝐙𝐄𝐙𝐄𝟒𝟕-𝐌𝐃 respond speed is*\n ```' +1000000990+ '``` *m/s*') 
+    return repondre('*•🛰️𝘡𝘌𝘡𝘌47 𝘛𝘌𝘊𝘏• ✍️ 𝐙𝐄𝐙𝐄𝟒𝟕-𝐌𝐃 respond speed is*\n ```' +1000000990+ '``` *m/s*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
