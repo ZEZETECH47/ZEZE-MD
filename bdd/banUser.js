b74441d0e9a2cbd5a8c60042d4b408f9f8a4bc0f
ZEZE47-MD <humphreymbise70@gmail.com>
2025-05-18 16:02:38 +0300

    Update banUser.js

--- bdd/banUser.js
+++ bdd/banUser.js
@@ -18,81 +18,77 @@ const proConfig = {
 // Créez une pool de connexions PostgreSQL
 const pool = new Pool(proConfig);
 
-// Vous pouvez maintenant utiliser 'pool' pour interagir avec votre base de données PostgreSQL.
-const creerTableBanUser = async () => {
+// Fonction pour créer la table "banGroup"
+const creerTableBanGroup = async () => {
   try {
     await pool.query(`
-      CREATE TABLE IF NOT EXISTS banUser (
-        jid text PRIMARY KEY
+      CREATE TABLE IF NOT EXISTS banGroup (
+        groupeJid text PRIMARY KEY
       );
     `);
-    console.log("La table 'banUser' a été créée avec succès.");
+    console.log("La table 'banGroup' a été créée avec succès.");
   } catch (e) {
-    console.error("Une erreur est survenue lors de la création de la table 'banUser':", e);
+    console.error("Une erreur est survenue lors de la création de la table 'banGroup':", e);
   }
 };
 
-// Appelez la méthode pour créer la table "banUser"
-creerTableBanUser();
+// Appelez la méthode pour créer la table "banGroup"
+creerTableBanGroup();
 
-
-
-// Fonction pour ajouter un utilisateur à la liste des bannis
-async function addUserToBanList(jid) {
+// Fonction pour ajouter un groupe à la liste des groupes bannis
+async function addGroupToBanList(groupeJid) {
   const client = await pool.connect();
   try {
-    // Insérez l'utilisateur dans la table "banUser"
-    const query = "INSERT INTO banUser (jid) VALUES ($1)";
-    const values = [jid];
+    // Insérez le groupe dans la table "banGroup"
+    const query = "INSERT INTO banGroup (groupeJid) VALUES ($1)";
+    const values = [groupeJid];
 
     await client.query(query, values);
-    console.log(`JID ${jid} ajouté à la liste des bannis.`);
+    console.log(`Groupe JID ${groupeJid} ajouté à la liste des groupes bannis.`);
   } catch (error) {
-    console.error("Erreur lors de l'ajout de l'utilisateur banni :", error);
+    console.error("Erreur lors de l'ajout du groupe banni :", error);
   } finally {
     client.release();
   }
 }
 
-
-
-// Fonction pour vérifier si un utilisateur est banni
-async function isUserBanned(jid) {
+// Fonction pour vérifier si un groupe est banni
+async function isGroupBanned(groupeJid) {
   const client = await pool.connect();
   try {
-    // Vérifiez si l'utilisateur existe dans la table "banUser"
-    const query = "SELECT EXISTS (SELECT 1 FROM banUser WHERE jid = $1)";
-    const values = [jid];
+    // Vérifiez si le groupe existe dans la table "banGroup"
+    const query = "SELECT EXISTS (SELECT 1 FROM banGroup WHERE groupeJid = $1)";
+    const values = [groupeJid];
 
     const result = await client.query(query, values);
     return result.rows[0].exists;
   } catch (error) {
-    console.error("Erreur lors de la vérification de l'utilisateur banni :", error);
+    console.error("Erreur lors de la vérification du groupe banni :", error);
     return false;
   } finally {
     client.release();
   }
 }
 
-// Fonction pour supprimer un utilisateur de la liste des bannis
-async function removeUserFromBanList(jid) {
+// Fonction pour supprimer un groupe de la liste des groupes bannis
+async function removeGroupFromBanList(groupeJid) {
   const client = await pool.connect();
   try {
-    // Supprimez l'utilisateur de la table "banUser"
-    const query = "DELETE FROM banUser WHERE jid = $1";
-    const values = [jid];
+    // Supprimez le groupe de la table "banGroup"
+    const query = "DELETE FROM banGroup WHERE groupeJid = $1";
+    const values = [groupeJid];
 
     await client.query(query, values);
-    console.log(`JID ${jid} supprimé de la liste des bannis.`);
+    console.log(`Groupe JID ${groupeJid} supprimé de la liste des groupes bannis.`);
   } catch (error) {
-    console.error("Erreur lors de la suppression de l'utilisateur banni :", error);
+    console.error("Erreur lors de la suppression du groupe banni :", error);
   } finally {
     client.release();
   }
 }
 
 module.exports = {
-  addUserToBanList,
-  isUserBanned,
-  removeUserFromBanList,
+  addGroupToBanList,
+  isGroupBanned,
+  removeGroupFromBanList,
 };
